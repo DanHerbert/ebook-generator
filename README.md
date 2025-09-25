1f7b4325efbea23a6d10155e2c21883a5eb4fb97
Dan Herbert <ebg-dan@hrbrt.co>
2025-09-25 10:48:26 -0400

    Functioning e-book generator code

--- README.md
+++ README.md
@@ -1,7 +1,39 @@
 # Ebook Generator
 
-Some code to take a single block of HTML and split it into chapters in an ePub file.
+Some code to take a big chunk of HTML and split it into chapters in an ePub file.
 
 Chapters are assumed to be delimited with either `<p>chapter</p>` or `<p><strong>chapter</strong></p>`.
 
-Used for some Patreon subscriptions which do not provide an ePub file but post their content as HTML.
+Used for one of my Patreon subscriptions to a book author which does not provide an ePub file but posts their work as HTML when published on Patreon.
+
+## Setup and Requirements
+
+I have only tested this app on Linux, but it should work on MacOS as well. Windows may be possible, but could require setting up WSL to use. Try at your own risk.
+
+This script requires Python to be installed on your system, as well as 2 other apps to be installed before running: "`rsvg-convert`" and "`ImageMagick`".
+
+`rsvg-convert` is available on most Linux/MacOS package managers under a few different names:
+
+* [`librsvg`](https://formulae.brew.sh/formula/librsvg) on MacOS/Linux Homebrew
+* [`librsvg2-bin`](https://packages.debian.org/stable/librsvg2-bin) on Debian-based distributions (Ubuntu, Mint, etc)
+* [`librsvg2-tools`](https://packages.fedoraproject.org/pkgs/librsvg2/librsvg2-tools/) on Red Hat-based distros
+* [`librsvg`](https://archlinux.org/packages/extra/x86_64/librsvg/) on Arch-based distros (Manjaro, SteamOS, etc)
+
+
+[ImageMagick](https://imagemagick.org/script/download.php) is available in most package managers as simply `imagemagick`.
+
+## Usage
+
+In the `/inputs/` folder, copy `metadata.example.yaml` as `metadata.yaml` in the same folder, filling in the details for each field. Copy `content.example.html` as `content.html` in the same folder, filling in the HTML code for the ebook you want to generate.
+
+Depending on your machine, once those files are ready you can simply run `./src/generate_ebook.py` from a terminal to generate the output. You may need to set up a Python venv by running the following commands:
+
+```sh
+python3 -m venv venv
+source venv/bin/activate
+pip install .
+```
+
+Once those commands finish you should be able to re-run the `./src/generate_ebook.py` script.
+
+When the script is finished, the file `book.epub` will be in the project's root.
---
+++ src/generate_ebook.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+
+import shutil
+import logging
+import shlex
+import subprocess
+import os
+import re
+import zipfile
+
+from string import Template
+
+import yaml
+
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+PROJECT_PATH = os.path.realpath(f"{SCRIPT_PATH}/..")
+METADATA_PATH = os.path.join(SCRIPT_PATH, "../inputs/metadata.yaml")
+EPUB_TEMPLATE_PATH = os.path.join(SCRIPT_PATH, "../epub-template")
+OUTPUT_PATH = os.path.join(SCRIPT_PATH, "../out_epub")
+
+
+def get_metadata():
+    """Gets the metadata from disk."""
+    with open(METADATA_PATH, mode="rt", encoding="utf-8") as file_handle:
+        return yaml.safe_load(file_handle)
+
+
+def real_join(a, *p):
+    return os.path.realpath(os.path.join(a, *p))
+
+
+def generate_file(filename, **mapping):
+    in_path = real_join(EPUB_TEMPLATE_PATH, filename)
+    out_path = real_join(OUTPUT_PATH, filename)
+    with open(in_path, "r", encoding="utf-8") as f:
+        file_tmpl = Template(f.read())
+    file_contents = file_tmpl.substitute(**mapping)
+    with open(out_path, "w", encoding="utf-8") as f:
+        f.write(file_contents)
+    print(f"Wrote {out_path.replace(PROJECT_PATH, '.')}")
+
+
+def generate_cover_jpg(metadata):
+    # rsvg-convert can only generate PNGs, but tools which can do direct SVG -> JPG
+    # conversion do not support all SVG features we need.
+    svg_path = real_join(OUTPUT_PATH, "cover.svg")
+    png_path = real_join(OUTPUT_PATH, "cover.png")
+    jpg_path = real_join(OUTPUT_PATH, "cover.jpg")
+    cmd_args = shlex.split(
+        f"rsvg-convert -h 1500 -w 938 --format png --output {png_path} {svg_path}"
+    )
+    subprocess.run(cmd_args)
+    subprocess.run(shlex.split(f"magick {png_path} {jpg_path}"))
+    os.remove(svg_path)
+    os.remove(png_path)
+    print(f"Wrote {jpg_path.replace(PROJECT_PATH, '.')}")
+
+
+def parse_book_contents(metadata):
+    content_path = real_join(SCRIPT_PATH, "../inputs/content.html")
+    with open(content_path, "r", encoding="utf-8") as f:
+        content = f.read()
+
+    # Split on the <p>chapter</p> marker
+    # This will give chunks of text between those markers
+    chapter_parts = re.split(
+        r"<p>?\s*(?:<strong>)?\s*chapter\s*(?:</strong>)?\s*</p>",
+        content,
+        flags=re.IGNORECASE,
+    )
+
+    filename = "chapter-template.xhtml"
+    in_path = real_join(EPUB_TEMPLATE_PATH, filename)
+
+    for i, chapter_content in enumerate(chapter_parts, start=1):
+        with open(in_path, "r", encoding="utf-8") as f:
+            chapter_tmpl = Template(f.read())
+        chapter_content = chapter_content.replace("&nbsp;", chr(0x00A0))
+        out_str = chapter_tmpl.substitute(
+            **metadata,
+            chapter_number_padded=f"{i:03}",
+            chapter_number=i,
+            chapter_content=chapter_content,
+        )
+        out_path = real_join(OUTPUT_PATH, f"chapter-{i:03}.xhtml")
+        with open(out_path, "w", encoding="utf-8") as f:
+            f.write(out_str)
+        print(f"Wrote {out_path.replace(PROJECT_PATH, '.')}")
+    return len(chapter_parts)
+
+
+def generate_metadata_opf(metadata, chapter_count):
+    filename = "metadata.opf"
+    in_path = real_join(EPUB_TEMPLATE_PATH, filename)
+    out_path = real_join(OUTPUT_PATH, filename)
+    with open(in_path, "r", encoding="utf-8") as f:
+        input_tmpl = Template(f.read())
+    chapter_item_list = ""
+    chapter_itemref_list = ""
+    for i in range(1, chapter_count + 1):
+        chapter_item_list += f'    <item id="chapter-{i:03}-xhtml" href="chapter-{i:03}.xhtml" media-type="application/xhtml+xml"/>\n'
+        chapter_itemref_list += f'    <itemref idref="chapter-{i:03}-xhtml"/>\n'
+    out_str = input_tmpl.substitute(
+        **metadata,
+        chapter_item_list=chapter_item_list.strip(),
+        chapter_itemref_list=chapter_itemref_list.strip(),
+    )
+    with open(out_path, "w", encoding="utf-8") as f:
+        f.write(out_str)
+    print(f"Wrote {out_path.replace(PROJECT_PATH, '.')}")
+
+
+def generate_page_toc(metadata, chapter_count):
+    filename = "page-toc.xhtml"
+    in_path = real_join(EPUB_TEMPLATE_PATH, filename)
+    out_path = real_join(OUTPUT_PATH, filename)
+    with open(in_path, "r", encoding="utf-8") as f:
+        input_tmpl = Template(f.read())
+    toc_chapters = ""
+    for i in range(1, chapter_count + 1):
+        toc_chapters += (
+            f'      <li><a href="chapter-{i:03}.xhtml">Chapter {i}</a></li>\n'
+        )
+    out_str = input_tmpl.substitute(
+        language_code=metadata["language_code"],
+        toc_chapters=toc_chapters.strip(),
+    )
+    with open(out_path, "w", encoding="utf-8") as f:
+        f.write(out_str)
+    print(f"Wrote {out_path.replace(PROJECT_PATH, '.')}")
+
+
+def generate_toc_ncx(metadata, chapter_count):
+    filename = "toc.ncx"
+    in_path = real_join(EPUB_TEMPLATE_PATH, filename)
+    out_path = real_join(OUTPUT_PATH, filename)
+    with open(in_path, "r", encoding="utf-8") as f:
+        input_tmpl = Template(f.read())
+    chapter_navpoints = ""
+    playOrderIdx = 4
+    for i in range(1, chapter_count + 1):
+        chapter_navpoints += f"""    <navPoint id="np-{i:03}" playOrder="{playOrderIdx}">
+      <navLabel>
+        <text>Chapter {i}</text>
+      </navLabel>
+      <content src="chapter-{i:03}.xhtml" />
+    </navPoint>\n"""
+        playOrderIdx += 1
+    out_str = input_tmpl.substitute(
+        **metadata,
+        chapter_navpoints=chapter_navpoints.strip(),
+    )
+    with open(out_path, "w", encoding="utf-8") as f:
+        f.write(out_str)
+    print(f"Wrote {out_path.replace(PROJECT_PATH, '.')}")
+
+
+def create_the_ebook():
+    zip_path = real_join(SCRIPT_PATH, "../book.epub")
+    with zipfile.ZipFile(zip_path, "w", zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(OUTPUT_PATH):
+            for file in files:
+                file_path = os.path.join(root, file)
+                # Store relative path inside the zip
+                arcname = os.path.relpath(file_path, start=OUTPUT_PATH)
+                zipf.write(file_path, arcname)
+    print(f"Wrote {zip_path.replace(PROJECT_PATH, '.')}")
+
+
+def main():
+    """Main app execution code."""
+    metadata = get_metadata()
+
+    if os.path.isdir(OUTPUT_PATH):
+        shutil.rmtree(OUTPUT_PATH)
+    shutil.copytree(
+        EPUB_TEMPLATE_PATH,
+        OUTPUT_PATH,
+        dirs_exist_ok=True,
+        copy_function=shutil.copy,
+    )
+    os.remove(os.path.join(OUTPUT_PATH, "chapter-template.xhtml"))
+
+    generate_file("cover.svg", **metadata)
+    generate_cover_jpg(metadata)
+    generate_file("page-cover.xhtml", **metadata)
+    generate_file("page-title.xhtml", **metadata)
+    generate_file("page-copyright.xhtml", **metadata)
+    chapter_count = parse_book_contents(metadata)
+    generate_metadata_opf(metadata, chapter_count)
+    generate_page_toc(metadata, chapter_count)
+    generate_toc_ncx(metadata, chapter_count)
+
+    create_the_ebook()
+
+
+if __name__ == "__main__":
+    main()
